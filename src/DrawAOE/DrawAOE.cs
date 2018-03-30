f7cd9085e556c0f5820310fdaa94691c729dcafb
Sharps <talkto.waldo@gmail.com>
2018-03-30 20:40:01 +0300

    Add files via upload

---
+++ src/DrawAOE/DrawAOE.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoeHUD.Plugins;
+using PoeHUD.Controllers;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.RemoteMemoryObjects;
+using PoeHUD.Hud.Menu.SettingsDrawers;
+using SharpDX;
+using ImGuiNET;
+using PoeHUD.Hud.UI;
+
+namespace DrawAOE
+{
+    internal class DrawAOE : BaseSettingsPlugin<DrawAOESettings>
+    {
+        private bool isTown;
+
+        public override void Initialise()
+        {
+            PluginName = "DrawAOE";
+
+            OnSettingsToggle();
+            Settings.Enable.OnValueChanged += OnSettingsToggle;
+        }
+
+        private void OnSettingsToggle()
+        {
+            try
+            {
+                if (Settings.Enable.Value)
+                {
+                    GameController.Area.OnAreaChange += OnAreaChange;
+                    GameController.Area.RefreshState();
+
+                    isTown = GameController.Area.CurrentArea.IsTown;
+
+
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+
+        public override void Render()
+        {
+
+            if (!Settings.Enable.Value) return;
+
+
+            if (Settings.CircleEnable.Value)
+            {
+                if (!Settings.DisplayInTown.Value && isTown)
+                {
+                    return;
+                }
+                else
+                {
+                    var pos = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Render>().Pos;
+                    DrawEllipseToWorld(pos, Settings.CircleSize.Value, 50, Settings.LineWidth.Value, Settings.LineColor.Value);
+                }
+            }
+
+            if (Settings.CircleEnable2.Value)
+            {
+                if (!Settings.DisplayInTown.Value && isTown)
+                {
+                    return;
+                }
+                else
+                {
+                    var pos = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Render>().Pos;
+                    DrawEllipseToWorld(pos, Settings.CircleSize2.Value, 50, Settings.LineWidth2.Value, Settings.LineColor2.Value);
+                }
+            }
+
+            if (Settings.CircleEnable3.Value)
+            {
+                if (!Settings.DisplayInTown.Value && isTown)
+                {
+                    return;
+                }
+                else
+                {
+                    var pos = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Render>().Pos;
+                    DrawEllipseToWorld(pos, Settings.CircleSize3.Value, 50, Settings.LineWidth3.Value, Settings.LineColor3.Value);
+                }
+            }
+
+            if (Settings.CircleEnable4.Value)
+            {
+                if (!Settings.DisplayInTown.Value && isTown)
+                {
+                    return;
+                }
+                else
+                {
+                    var pos = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Render>().Pos;
+                    DrawEllipseToWorld(pos, Settings.CircleSize4.Value, 50, Settings.LineWidth4.Value, Settings.LineColor4.Value);
+                }
+            }
+        }
+
+        private void OnAreaChange(AreaController area)
+        {
+            if (Settings.Enable.Value)
+            {
+                isTown = area.CurrentArea.IsTown;
+            }
+        }
+
+
+        public void DrawEllipseToWorld(Vector3 vector3Pos, int radius, int points, int lineWidth, Color color)
+        {
+            var camera = GameController.Game.IngameState.Camera;
+
+            var plottedCirclePoints = new List<Vector3>();
+            var slice = 2 * Math.PI / points;
+            for (var i = 0; i < points; i++)
+            {
+                var angle = slice * i;
+                var x = (decimal)vector3Pos.X + decimal.Multiply((decimal)radius, (decimal)Math.Cos(angle));
+                var y = (decimal)vector3Pos.Y + decimal.Multiply((decimal)radius, (decimal)Math.Sin(angle));
+                plottedCirclePoints.Add(new Vector3((float)x, (float)y, vector3Pos.Z));
+            }
+
+            var rndEntity = GameController.Entities.FirstOrDefault(x =>
+                x.HasComponent<Render>() && GameController.Player.Address != x.Address);
+
+            for (var i = 0; i < plottedCirclePoints.Count; i++)
+            {
+                if (i >= plottedCirclePoints.Count - 1)
+                {
+                    var pointEnd1 = camera.WorldToScreen(plottedCirclePoints.Last(), rndEntity);
+                    var pointEnd2 = camera.WorldToScreen(plottedCirclePoints[0], rndEntity);
+                    Graphics.DrawLine(pointEnd1, pointEnd2, lineWidth, color);
+                    return;
+                }
+
+                var point1 = camera.WorldToScreen(plottedCirclePoints[i], rndEntity);
+                var point2 = camera.WorldToScreen(plottedCirclePoints[i + 1], rndEntity);
+                Graphics.DrawLine(point1, point2, lineWidth, color);
+            }
+        }
+    }
+}
---
+++ src/DrawAOE/DrawAOESettings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoeHUD.Plugins;
+using PoeHUD.Hud.Settings;
+using SharpDX;
+
+namespace DrawAOE
+{
+    internal class DrawAOESettings : SettingsBase
+    {
+        public DrawAOESettings()
+        {
+            //plugin itself
+            Enable = false;
+            DisplayInTown = false;
+
+            CircleEnable = true;
+            CircleSize = new RangeNode<int>(500, 50, 2000);
+            LineWidth = new RangeNode<int>(1, 1, 5);
+            LineColor = new ColorBGRA(0, 255, 255, 255);
+
+            CircleEnable2 = false;
+            CircleSize2 = new RangeNode<int>(500, 50, 2000);
+            LineWidth2 = new RangeNode<int>(1, 1, 5);
+            LineColor2 = new ColorBGRA(0, 255, 255, 255);
+
+            CircleEnable3 = false;
+            CircleSize3 = new RangeNode<int>(500, 50, 2000);
+            LineWidth3 = new RangeNode<int>(1, 1, 5);
+            LineColor3 = new ColorBGRA(0, 255, 255, 255);
+
+            CircleEnable4 = false;
+            CircleSize4 = new RangeNode<int>(500, 50, 2000);
+            LineWidth4 = new RangeNode<int>(1, 1, 5);
+            LineColor4 = new ColorBGRA(0, 255, 255, 255);
+
+        }
+
+        //Menu
+
+
+        [Menu("Display in Town", 1)]
+        public ToggleNode DisplayInTown { get; set; }
+
+        [Menu("Circle 1", 2)]
+        public ToggleNode CircleEnable { get; set; }
+        [Menu("Circle Size", 20, 2)]
+        public RangeNode<int> CircleSize { get; set; }
+        [Menu("Line Width", 21, 2)]
+        public RangeNode<int> LineWidth { get; set; }
+        [Menu("Line Color", 22, 2)]
+        public ColorNode LineColor { get; set; }
+
+        [Menu("Circle 2", 3)]
+        public ToggleNode CircleEnable2 { get; set; }
+        [Menu("Circle Size", 30, 3)]
+        public RangeNode<int> CircleSize2 { get; set; }
+        [Menu("Line Width", 31, 3)]
+        public RangeNode<int> LineWidth2 { get; set; }
+        [Menu("Line Color", 32, 3)]
+        public ColorNode LineColor2 { get; set; }
+
+        [Menu("Circle 3", 4)]
+        public ToggleNode CircleEnable3 { get; set; }
+        [Menu("Circle Size", 40, 4)]
+        public RangeNode<int> CircleSize3 { get; set; }
+        [Menu("Line Width", 41, 4)]
+        public RangeNode<int> LineWidth3 { get; set; }
+        [Menu("Line Color", 42, 4)]
+        public ColorNode LineColor3 { get; set; }
+
+        [Menu("Circle 4", 5)]
+        public ToggleNode CircleEnable4 { get; set; }
+        [Menu("Circle Size", 50, 5)]
+        public RangeNode<int> CircleSize4 { get; set; }
+        [Menu("Line Width", 51, 5)]
+        public RangeNode<int> LineWidth4 { get; set; }
+        [Menu("Line Color", 52, 5)]
+        public ColorNode LineColor4 { get; set; }
+
+    }
+}
